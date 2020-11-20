346745f0348cc979543685cc81eeb257e5eab51d
Davide Pizzato <davidepizzato@webmapp.it>
2020-11-20 11:24:53 +0100

    Added new parameters to filter features at loading

--- shortcodes/wm-embedmaps-elevation-chart.php
+++ shortcodes/wm-embedmaps-elevation-chart.php
@@ -5,10 +5,17 @@ function wm_render_elevation_chart_shortcode($atts)
 {
     ob_start();
 
-    // extract(shortcode_atts(array(
-    // ), $atts));
+    extract(shortcode_atts(array(
+        "feature_id" => ""
+    ), $atts));
+
+    $htmlAttributes = '';
+
+    if ($feature_id != '') {
+        $htmlAttributes .= " featureId=\"$feature_id\"";
+    }
     ?>
-    <wm-embedmaps-elevation-chart></wm-embedmaps-elevation-chart>
+    <wm-embedmaps-elevation-chart <?php echo $htmlAttributes; ?>></wm-embedmaps-elevation-chart>
     <?php
 $html = ob_get_clean();
     return $html;
--- shortcodes/wm-embedmaps-technical-info.php
+++ shortcodes/wm-embedmaps-technical-info.php
@@ -8,6 +8,7 @@ function wm_render_technical_info_shortcode($atts)
     extract(shortcode_atts(array(
         "related_poi" => "",
         "config" => "",
+        "feature_id" => ""
     ), $atts));
 
     $htmlAttributes = '';
@@ -16,7 +17,10 @@ function wm_render_technical_info_shortcode($atts)
         $htmlAttributes .= " wm-related-poi=\"$related_poi\"";
     }
     if ($config != '') {
-        $htmlAttributes .= " wm-config=\"$config\"";
+        $htmlAttributes .= " config=\"$config\"";
+    }
+    if ($feature_id != '') {
+        $htmlAttributes .= " featureId=\"$feature_id\"";
     }
     ?>
     <wm-embedmaps-technical-info <?php echo $htmlAttributes; ?>></wm-embedmaps-technical-info>
